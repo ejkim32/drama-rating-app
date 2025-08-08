990d21e25a0199da965367318b1b995b648c234a
ejkim32 <hi427454@gmail.com>
2025-08-08 12:00:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import MultiLabelBinarizer
+import scipy.stats as stats
 
 # 한글 폰트 설정 (Windows: Malgun Gothic, macOS/Linux는 적절한 한글 폰트로)
 # 1) 사용할 한글 폰트 이름 설정

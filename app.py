49fcf1a700ab76fe3ec5ac92d091bdb36398663d
ejkim32 <hi427454@gmail.com>
2025-08-11 09:56:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import ast
 import matplotlib
+import platform
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler
 from sklearn.metrics import r2_score, mean_squared_error
@@ -16,9 +17,15 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression
 import plotly.express as px
 
-# 한글 폰트 설정
-matplotlib.rcParams['font.family'] = 'DejaVu Sans'
-matplotlib.rcParams['axes.unicode_minus'] = False 
+# OS별 기본 한글 폰트 설정
+if platform.system() == 'Windows':
+    matplotlib.rcParams['font.family'] = 'Malgun Gothic'  # 윈도우
+elif platform.system() == 'Darwin':  # macOS
+    matplotlib.rcParams['font.family'] = 'AppleGothic'
+else:  # Linux/Colab
+    matplotlib.rcParams['font.family'] = 'NanumGothic'
+
+matplotlib.rcParams['axes.unicode_minus'] = False  # 마이너스 깨짐 방지
 
 # =========================
 # 0. 유틸리티 함수 및 클래스

63736663ebfbc8208259a2434efa59d687a7410e
ejkim32 <hi427454@gmail.com>
2025-08-12 21:29:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def _inject_sparrow_css():
     st.markdown("""
     <style>
       /* ---------- Layout / Typography ---------- */
-      .block-container{padding-top:0.8rem; padding-bottom:2rem;}
+      .block-container{padding-top:0.8rem; padding-bottom:3rem;}
       h1,h2,h3{font-weight:800;}
       /* ---------- Topbar ---------- */
       .topbar{display:flex; align-items:flex-end; justify-content:space-between; margin:6px 0 14px;}
